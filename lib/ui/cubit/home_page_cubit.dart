31cd1b71418dc3d6467ac90832ea2069b67d469b
Ayberk Bayam <bayam20cp@gmail.com>
2026-02-26 16:01:49 +0300

    refactor: home_page_cubit update.

--- lib/ui/cubit/home_page_cubit.dart
+++ lib/ui/cubit/home_page_cubit.dart
@@ -1,19 +1,15 @@
+import 'package:bloc_learning/data/repo/math_repo.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomePageCubit extends Cubit<int> {
   HomePageCubit() : super(0);
 
+  var mrepo = MathRepo();
   void addUp(String fetchNum1, String fetchNum2) {
-    int number1 = int.parse(fetchNum1);
-    int number2 = int.parse(fetchNum2);
-    int sum = number1 + number2;
-    emit(sum); //*Tetikleyici ve veri gönderme.
+    emit(mrepo.addUp(fetchNum1, fetchNum2));
   }
 
   void addmultiplicationUpUp(String fetchNum1, String fetchNum2) {
-    int number1 = int.parse(fetchNum1);
-    int number2 = int.parse(fetchNum2);
-    int sum = number1 * number2;
-    emit(sum); //*Tetikleyici ve veri gönderme.
+    emit(mrepo.addmultiplicationUpUp(fetchNum1, fetchNum2));
   }
 }
